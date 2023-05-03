d63451138cfe646b732e59cc4bebc4e32286e809
Alexandre Rocha Coutinho <alex.rochacoutinho@gmail.com>
2023-05-03 14:35:23 +0200

    Organized Folders

--- Assets/ObstacleSpawner.cs
+++ Assets/ObstacleSpawner.cs
@@ -12,9 +12,17 @@ public class ObstacleSpawner : NinjaMonoBehaviour {
     private IEnumerator SpawnObstacleRoutine() {
         string logId = "SpawnObstacleRoutine";
         var obstaclePoolsCount = obstaclePools.Count;
+        if(obstaclePoolsCount==0) {
+            loge(logId, "No pools found => no-op");
+            yield break;
+        }
         while(true) {
-            var randomPool = Random.Range(0, obstaclePoolsCount);
-            obstaclePools[randomPool].GetPooledObject();
+            var randomPool = obstaclePools[Random.Range(0, obstaclePoolsCount)];
+            if(randomPool==null) {
+                logw(logId, "RandomPool="+randomPool.logf()+" => continuing");
+                continue;
+            }
+            randomPool.GetPooledObject();
             yield return new WaitForSeconds(Random.Range(0.25f, 1.25f));
         }
     }
