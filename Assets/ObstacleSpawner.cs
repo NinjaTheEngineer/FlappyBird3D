f2ffc75813a95294dafad57c39ca4856c4af511a
Alexandre Rocha Coutinho <alex.rochacoutinho@gmail.com>
2023-05-03 10:29:01 +0200

    Implemented initial ObstacleSpawner
    Switched build to Android

--- Assets/ObstacleSpawner.cs
+++ Assets/ObstacleSpawner.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 using NinjaTools;
 
 public class ObstacleSpawner : NinjaMonoBehaviour {
-    public GameObject obstaclePrefab;
+    public List<ObjectPool> obstaclePools;
+    private void Start() {
+        StartCoroutine(SpawnObstacleRoutine());
+    }
+
+    private IEnumerator SpawnObstacleRoutine() {
+        string logId = "SpawnObstacleRoutine";
+        var obstaclePoolsCount = obstaclePools.Count;
+        while(true) {
+            var randomPool = Random.Range(0, obstaclePoolsCount);
+            obstaclePools[randomPool].GetPooledObject();
+            yield return new WaitForSeconds(Random.Range(0.25f, 1.25f));
+        }
+    }
     
 }
